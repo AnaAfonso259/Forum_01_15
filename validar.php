61b014ea2885a3e1429a2e6e725b602fb286b936
AnaAfonso259 <184397960+AnaAfonso259@users.noreply.github.com>
2024-11-29 10:45:12 +0000

    exercício 5, primeiro ponto

--- validar.php
+++ validar.php
@@ -10,12 +10,17 @@
                 $_SESSION['erro'] = 'Credenciais erradas';
                 header('Location: login.php');
             }
+            else if($rs['ativo'] == 0){
+                $_SESSION['erro'] = 'Conta Desativada, contacte o admin';
+                header('Location: login.php');
+            }
             else {
                 $_SESSION['idutilizador'] = $rs['idutilizador'];
                 $_SESSION['utilizador'] = $rs['utilizador'];
                 $_SESSION['tipo'] = $rs['tipo']; // 0 admin, 1 utilizador
                 header('Location: topicos.php');
             }
+          
         }
     }
 ?>
